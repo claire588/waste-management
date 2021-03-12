7a37769feadc046412dc1521665910c6847ec4e1
Claire <cchri039@uottawa.ca>
2021-03-11 20:46:31 -0500

    Game and Home

---
+++ Claire/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+
+    public void Game()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+        //Debug.Break();                                            
+    }
+    public void Back()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Game and Home/Assets/Scenes/waste-management-Home/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GotoHomeScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    // Update is called once per frame
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
