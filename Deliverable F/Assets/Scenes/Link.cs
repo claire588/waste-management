b460a412f5a765649ed7b55d8db61195326adc72
unknown <iphil098@uottawa.ca>
2021-03-17 10:13:22 -0400

    Search

---
+++ Deliverable F/Assets/Scenes/Link.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+  public void ottawar(){
+        Application.OpenURL("https://app06.ottawa.ca/online_services/recycling/items/443_en.html");
+  }
+}
+
+/*
+  // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
